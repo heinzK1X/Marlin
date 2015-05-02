6312e2cb74fb81c88478edd5dc80fe1a338dae01
Heiner Tholen <demo01@heinertholen.com>
2015-05-02 19:43:22 +0200

    reset to small feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -525,7 +525,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 // delta speeds must be the same on xyz
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {222, 222, 222, 1120*1.0}  // default for richrap: 55.5  (x4 for microstepping)
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {55.5, 55.5, 55.5, 280*1.0}  // default for richrap: 55.5 
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 500, 29}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,9000,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
